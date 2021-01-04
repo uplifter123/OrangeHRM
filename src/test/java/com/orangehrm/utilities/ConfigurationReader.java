d569bcded977e116595244ab053d09a016cb7272
SynergyO <synergyon10@gmail.com>
2021-01-04 14:27:33 +0100

    last update

--- src/test/java/com/orangehrm/utilities/ConfigurationReader.java
+++ src/test/java/com/orangehrm/utilities/ConfigurationReader.java
@@ -1,5 +1,9 @@
 package com.orangehrm.utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 import java.io.FileInputStream;
 import java.util.Properties;
 
@@ -19,4 +23,21 @@ public class ConfigurationReader {
     public static String getProperty(String key){
         return properties.getProperty(key);
     }
+
+    public static void jobTitleHoverClick(String jbTitle){
+        //td[@name='listField.name']//span[contains(text(),'IT Manager')]
+        String beforeXpath="//td[@name='listField.name']//span[contains(text(),'";
+        String afterXpath="')]";
+        WebElement viewButton= Driver.getDriver().findElement(By.xpath(beforeXpath + jbTitle + afterXpath));
+        //hover Element
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(viewButton).click().perform();
+
+    }
+
+
+
+
+
+
 }
